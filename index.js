cd99ad0d098753e7e2f9c84c39d582ef94e299ef
Alex Valdivia <alexvaldivia@gmail.com>
2018-03-26 23:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  return push.kittens()
+  kittens.push(name)
+  return kittens
 }
